b1e7d0f4f063c3a3fd2c12b7858a6b836147531a
vijay-git08 <vijayitsme6@gmail.com>
2022-10-02 13:08:12 +0530

    second commit git

---
+++ src/gitchecktwo/CheckTwo.java
@@ -0,0 +1,19 @@
+package gitchecktwo;
+
+public class CheckTwo {
+	
+	public static void main(String[] args) {  
+		int x=40;
+		int y=20;
+		if(x+y>260) {
+			System.out.println("x+y is greater than 20");
+		}
+		else {
+			System.out.println("+y is not greater");
+		}
+		
+}
+}
+
+
+}
